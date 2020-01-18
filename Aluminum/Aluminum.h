868e6600aec21e29a4b7bcac649aea3435e894fa
Maxim Vainshtein <maximv88@gmail.com>
2020-01-18 13:57:48 +0200

    Initial Commit

---
+++ Aluminum/Aluminum.h
@@ -0,0 +1,19 @@
+//
+//  Aluminum.h
+//  Aluminum
+//
+//  Created by Maxim Vainshtein on 18/01/2020.
+//  Copyright © 2020 Maxim Vainshtein. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Aluminum.
+FOUNDATION_EXPORT double AluminumVersionNumber;
+
+//! Project version string for Aluminum.
+FOUNDATION_EXPORT const unsigned char AluminumVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Aluminum/PublicHeader.h>
+
+
